5140c30c9a05c4e01d3e2f7298389ea1af4cb3eb
Gustavo Dau Pimenta <gustavo-dau@hotmail.com>
2023-01-12 14:46:55 -0300

    Exercício

---
+++ Funcao Maior Valor.py
@@ -0,0 +1,24 @@
+#Faça um programa que tenha uma função chamada maior(), que receba vários parâmetros com valores inteiros.
+# Seu programa tem que analisar todos os valores e dizer qual deles é o maior.
+from time import sleep
+
+
+def maior(* num):
+    cont = maior = 0
+    print('-=' * 20)
+    print('\n analisando os valores passados...')
+    for valor in num:
+        print(f'{valor} ', end='', flush=True)
+        sleep(0.3)
+        if cont == 0:
+            maior = valor
+        else:
+            if valor > maior:
+                maior = valor
+        cont += 1
+    print(f'Foram informados {cont} valores ao todo')
+    print(f'o maior valor informado foi {maior}')
+
+
+maior(3, 5, 8)
+maior(90,88,77,88)
